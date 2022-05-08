2ad646438b5dc168907da078d66e8926227bd7eb
rychu-tech <rychudotka14@gmail.com>
2022-05-08 15:42:44 +0200

    Not working fragment attempt

--- app/src/main/java/com/example/hugeprojectlabs/MainActivity.kt
+++ app/src/main/java/com/example/hugeprojectlabs/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.hugeprojectlabs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.fragment.app.ListFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val database = DatabaseHelper(applicationContext)
-        val text_view = findViewById<TextView>(R.id.test)
+
+        //val text_view = findViewById<TextView>(R.id.test)
+
+        supportFragmentManager!!.
+        beginTransaction().
+        add(R.id.fragmentContainer, ListFragment(), "ListFragment").commit()
 
 
     }
---
+++ app/src/main/java/com/example/hugeprojectlabs/TrasaFragment.kt
@@ -0,0 +1,42 @@
+package com.example.hugeprojectlabs
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class TrasaFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var goToButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_trasa, container, false)
+        goToButton = rootView.findViewById(R.id.buttonTrasa)
+        goToButton.setOnClickListener{
+            val fm = fragmentManager
+
+            fm!!.beginTransaction()
+                .remove(this)
+                .addToBackStack(null).commit()
+        }
+        return rootView
+    }
+
+}
